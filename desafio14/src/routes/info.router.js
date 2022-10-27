3326d5243dd1047f4059552c959ac39b472b8687
mxcaamano95 <mxcaamano@outlook.com>
2022-10-26 23:53:08 -0300

    desafio14 - Analisis de performance 1) Actualizado, pendiente 2) y 3)

--- desafio14/src/routes/info.router.js
+++ desafio14/src/routes/info.router.js
@@ -3,9 +3,24 @@ const routerInfo = Router();
 const CPUqty = require('os').cpus().length;
 const logger = require('../utils/logger')
 
+//Ruta utilizada para probar no bloqueante
+// routerInfo.get('/', (req, res) => {
+//     logger.info(`Ruta: ${req.originalUrl}, Método: ${req.method}`)
+//     res.status(200).json({
+//         argv: process.argv.slice(2),
+//         SO: process.platform,
+//         version: process.version,
+//         memory: process.memoryUsage(),
+//         execPath: process.execPath,
+//         proyectPath:process.cwd(),
+//         processID: process.pid,
+//         CPUqty: CPUqty
+//     })
+// })
+
 routerInfo.get('/', (req, res) => {
-    logger.info(`Ruta: ${req.originalUrl}, Método: ${req.method}`)
-    res.status(200).json({
+    //     logger.info(`Ruta: ${req.originalUrl}, Método: ${req.method}`)
+    const info = {
         argv: process.argv.slice(2),
         SO: process.platform,
         version: process.version,
@@ -14,7 +29,19 @@ routerInfo.get('/', (req, res) => {
         proyectPath:process.cwd(),
         processID: process.pid,
         CPUqty: CPUqty
-    })
+    }
+    //Utilizado para probar bloqueante
+    console.log(
+        `argv: ${info.argv}` + "\n",
+        `SO: ${info.SO}` + "\n",
+        `version: ${info.version}` + "\n",
+        `memory: ${info.memory}` + "\n",
+        `execPath: ${info.execPath}` + "\n",
+        `proyectPath: ${info.proyectPath}` + "\n",
+        `processID: ${info.processID}` + "\n",
+        `CPUqty: ${info.CPUqty}` + "\n",
+    );
+    res.status(200).send(info)
 })
 
 module.exports = { routerInfo, CPUqty };
