09b48c8737b1a9263df28130eae6e2bb2f45d886
mxcaamano95 <mxcaamano@outlook.com>
2022-10-27 20:52:43 -0300

    desafio14

--- desafio14/src/routes/info.router.js
+++ desafio14/src/routes/info.router.js
@@ -18,7 +18,7 @@ const logger = require('../utils/logger')
 // })
 
 routerInfo.get('/', (req, res) => {
-    //     logger.info(`Ruta: ${req.originalUrl}, Método: ${req.method}`)
+        logger.info(`Ruta: ${req.originalUrl}, Método: ${req.method}`)
     const info = {
         argv: process.argv.slice(2),
         SO: process.platform,
@@ -30,16 +30,16 @@ routerInfo.get('/', (req, res) => {
         CPUqty: CPUqty
     }
     //Utilizado para probar bloqueante
-    console.log(
-        `argv: ${info.argv}` + "\n",
-        `SO: ${info.SO}` + "\n",
-        `version: ${info.version}` + "\n",
-        `memory: ${info.memory}` + "\n",
-        `execPath: ${info.execPath}` + "\n",
-        `proyectPath: ${info.proyectPath}` + "\n",
-        `processID: ${info.processID}` + "\n",
-        `CPUqty: ${info.CPUqty}` + "\n",
-    );
+    // console.log(
+    //     `argv: ${info.argv}` + "\n",
+    //     `SO: ${info.SO}` + "\n",
+    //     `version: ${info.version}` + "\n",
+    //     `memory: ${info.memory}` + "\n",
+    //     `execPath: ${info.execPath}` + "\n",
+    //     `proyectPath: ${info.proyectPath}` + "\n",
+    //     `processID: ${info.processID}` + "\n",
+    //     `CPUqty: ${info.CPUqty}` + "\n",
+    // );
     res.status(200).send(info)
 })
 
