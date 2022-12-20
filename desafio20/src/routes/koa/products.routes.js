e21add65c797c3a16861d9da08f4c5d2fd102146
mxcaamano95 <mxcaamano@outlook.com>
2022-12-19 23:10:38 -0300

    desafio20

--- desafio20/src/routes/koa/products.routes.js
+++ desafio20/src/routes/koa/products.routes.js
@@ -3,7 +3,7 @@ const router = new Router();
 const { getProducts, getProductById, addProduct, updateProduct, deleteProduct, deleteAllProducts } = require('../../controllers/koa/products.controller')
 const { authMiddleware } = require('../../middlewares/middlewaresKoa');
 
-router.get('/productos', authMiddleware, getProducts)
+router.get('/productos', authMiddleware,  getProducts)
 router.get('/productos/:id', authMiddleware, getProductById)
 router.post('/productos', authMiddleware, addProduct)
 router.put('/productos/:id', authMiddleware, updateProduct)
